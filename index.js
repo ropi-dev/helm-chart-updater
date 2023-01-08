17904b9fa8a9625c3be4a427cc041b9ec25bcc65
László Stahorszki <laszlo.stahorszki@ropi.dev>
2023-01-08 15:19:37 +0100

    Fix ESModule scripting

--- index.js
+++ index.js
@@ -1,16 +1,124 @@
-const jwt = require('jsonwebtoken')
+import jwt from 'jsonwebtoken'
+import storage from '@google-cloud/storage'
+import functions from '@google-cloud/functions-framework'
+import fetch from 'node-fetch'
+import fs from "fs";
 
 const generateJwt = (appId, secret) => {
     const data = {
-        'iat': Date.now(),
-        'exp': Date.now() + 60,
+        'iat': Math.round(Date.now() / 1000),
+        'exp': Math.round(Date.now() / 1000) + 60,
         'iss': appId
     };
-    return jwt.sign(data, secret, { algorithm: 'RS256' })
+    return jwt.sign(data, secret, {algorithm: 'RS256'})
 }
 
-exports.main = (req, res) => {
-    res.status(204)
+const getInstallationAccessToken = async (jwt, installation) => {
+    return fetch(`https://api.github.com/app/installations/${installation}/access_tokens`, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${jwt}`,
+            "Accept": "application/vnd.github+json"
+        }
+    }).then(response => response.json())
+        .then(response => response["token"])
 }
 
-exports.generateJwt = generateJwt
+const getInstallationId = async (jwt, accountId) => {
+    return fetch("https://api.github.com/app/installations", {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${jwt}`,
+            "Accept": "application/vnd.github+json"
+        }
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject({
+            status: response.status,
+            statusText: response.statusText,
+        })
+    })
+        .then(response => response.find(item => String(item["account"]["id"]) === accountId))
+        .then(response => response.id)
+}
+
+const getLatestReleaseAssetsUrl = async (token, repo) => {
+    return fetch(`https://api.github.com/repos/ropi-dev/${repo}/releases`, {
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Accept": "application/vnd.github+json"
+        }
+    })
+        .then(response => response.json())
+        .then(response => response[0])
+        .then(response => response['assets_url'])
+}
+
+const getLatestReleasePackageDownloadUrl = async (token, url) => {
+    return fetch(url, {
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Accept": "application/vnd.github+json"
+        }
+    })
+        .then(response => response.json())
+        .then(response => response.find(item => item["name"].endsWith(".tgz")))
+        .then(response => ({
+            url: response["url"],
+            name: response["name"]
+        }))
+}
+
+const handler = async (req, res) => {
+    const jwt = generateJwt(process.env.APPID, process.env.PRIVKEY)
+
+    await getInstallationId(jwt, process.env.ACCID)
+        .then(async (id) => await getInstallationAccessToken(jwt, id))
+        .then(async (token) => ({token, url: await getLatestReleaseAssetsUrl(token, "tsc-base-service-chart")}))
+        .then(async (result) => ({...result, ...await getLatestReleasePackageDownloadUrl(result.token, result.url)}))
+        .then(async (result) => await downloadChart(result))
+        .then(file => res.status(200).send(file))
+        .catch(error => res.status(500).send(error))
+};
+functions.http('main', handler);
+
+async function downloadIndex(from, filename) {
+    const client = new storage.Storage();
+
+    return client
+        .bucket(from)
+        .file(filename)
+        .copy(filename)
+        .finally(res => {
+            console.log("------------------ FILE ------------------")
+            console.log(res)
+        });
+}
+
+const downloadChart = async (download) => {
+    try {
+        const res = await fetch(download.url, {
+            headers: {
+                "Authorization": `Bearer ${download.token}`,
+                "Accept": "application/octet-stream"
+            },
+            follow: 10
+        });
+        const fileStream = fs.createWriteStream(download.name);
+        return new Promise((resolve, reject) => {
+            res.body.pipe(fileStream);
+            res.body.on("error", reject);
+            fileStream.on("finish", resolve);
+        }).then(() => download.name);
+    } catch (e) {
+        return Promise.reject(e)
+    }
+
+}
+
+export {
+    generateJwt,
+    handler
+}
--- index.test.js
+++ index.test.js
@@ -1,8 +1,8 @@
-const index = require("./index")
-const jwt = require("jsonwebtoken")
-const fs = require("fs")
+import * as index from "./index.js"
+import jwt from "jsonwebtoken"
+import fs from "fs"
 
-test('a JWT token is generated', () => {
+test.only('a JWT token is generated', () => {
     const privkey = fs.readFileSync("./privkey.pem")
     const pubkey = fs.readFileSync("./pubkey.pem")
     const result = index.generateJwt("279058", privkey);
@@ -10,3 +10,17 @@ test('a JWT token is generated', () => {
     expect(result).toMatch(/^ey.*/);
     expect(jwt.verify(result, pubkey, {algorithm: "RS256"})).toBeTruthy()
 });
+
+test('call github api', async () => {
+    process.env.APPID = "279058"
+    process.env.ACCID = "117531988"
+    if(!process.env.PRIVKEY) {
+        process.env.PRIVKEY = fs.readFileSync("./privkey.pem")
+    }
+
+    await index.handler({}, {
+        status: () => ({
+            send: (s) => console.log(s)
+        })
+    })
+})
